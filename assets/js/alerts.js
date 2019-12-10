5888d94895eab35a1e49595a639bfc8a8f02f37e
Fernando Herrera <fernando.herrera85@gmail.com>
2019-12-10 14:27:05 -0500

    Fin sección 2

---
+++ assets/js/alerts.js
@@ -0,0 +1,15 @@
+
+
+
+alert('Hola Mundo');
+
+
+let nombre = prompt('¿Cuál es tu nombre?');
+console.log( nombre );
+console.log( '****' + nombre + '****' ); // ''
+
+const seleccion = confirm('¿Está seguro de borrar esto?');
+console.log( seleccion );
+
+// console.log( global );
+
---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+// alert('Hola desde app.js');
+
+
+
+// console.log( console.log('Hola Mundo') );
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo =  c + d;
+
+// console.log('%c Mis variables', 'color:blue; font-weight: bold');
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+c = 'Hola de nuevo';
+
+// console.table({a, b, c, d, x });
+
+let miNuevoNombre = 'Fernando Herrera';
